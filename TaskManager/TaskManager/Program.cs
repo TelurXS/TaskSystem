75c60cb5480a06ccf8421e43d5fce9e2d44f1902
TurBinXS <zawgorodnioleg@gmail.com>
2022-07-10 22:12:54 +0300

    Removed test lines. Removed Task.json

--- TaskManager/TaskManager/Program.cs
+++ TaskManager/TaskManager/Program.cs
@@ -11,80 +11,74 @@ namespace TaskSystem
     {
         public static void Main() 
         {
-            //while (true)
-            //{
-            //    Console.WriteLine("=======================================");
-            //    Console.WriteLine("Select an action:");
-            //    Console.WriteLine("1. Create a task");
-            //    Console.WriteLine("2. Edit task");
-            //    Console.WriteLine("3. Delete task");
-            //    Console.WriteLine("4. Show task");
-            //    Console.WriteLine("=======================================");
+            while (true)
+            {
+                Console.WriteLine("=======================================");
+                Console.WriteLine("Select an action:");
+                Console.WriteLine("1. Create a task");
+                Console.WriteLine("2. Edit task");
+                Console.WriteLine("3. Delete task");
+                Console.WriteLine("4. Show task");
+                Console.WriteLine("=======================================");
 
-            //    string choice = Console.ReadLine();
-            //    TaskManager manager = new TaskManager();
-            //    DefenseFool defenseFool = new DefenseFool();
+                string choice = Console.ReadLine();
+                TaskManager manager = new TaskManager();
+                DefenseFool defenseFool = new DefenseFool();
 
 
-            //    if (choice == "1")
-            //    {
-            //        manager.SetTasks(TaskManager.Load());  // Loading information from json file
+                if (choice == "1")
+                {
+                    manager.SetTasks(TaskManager.Load());  // Loading information from json file
 
-            //        Console.Write("Enter note: ");
-            //        string message = Console.ReadLine();
+                    Console.Write("Enter note: ");
+                    string message = Console.ReadLine();
 
-            //        DateTime date = defenseFool.NewDate();
+                    DateTime date = defenseFool.NewDate();
 
-            //        manager.AddTask(message, date);
-            //        manager.Save();
-            //    }
-            //    else if (choice == "2")
-            //    {
-            //        manager.SetTasks(TaskManager.Load());
+                    manager.AddTask(message, date);
+                    manager.Save();
+                }
+                else if (choice == "2")
+                {
+                    manager.SetTasks(TaskManager.Load());
 
-            //        Console.Write("Enter note: ");
-            //        string oldMessage = Console.ReadLine();
+                    Console.Write("Enter note: ");
+                    string oldMessage = Console.ReadLine();
 
-            //        DateTime oldDate = defenseFool.NewDate();
+                    DateTime oldDate = defenseFool.NewDate();
 
-            //        Console.Write("Enter new note: ");
-            //        string newMessage = Console.ReadLine();
+                    Console.Write("Enter new note: ");
+                    string newMessage = Console.ReadLine();
 
-            //        DateTime newDate = defenseFool.NewDate();
+                    DateTime newDate = defenseFool.NewDate();
 
-            //        manager.EditTask(oldMessage, oldDate, newMessage, newDate);
-            //        manager.Save();
-            //    }
-            //    else if (choice == "3")
-            //    {
-            //        manager.SetTasks(TaskManager.Load());
+                    manager.EditTask(oldMessage, oldDate, newMessage, newDate);
+                    manager.Save();
+                }
+                else if (choice == "3")
+                {
+                    manager.SetTasks(TaskManager.Load());
 
-            //        Console.Write("Enter note: ");
-            //        string message = Console.ReadLine();
+                    Console.Write("Enter note: ");
+                    string message = Console.ReadLine();
 
-            //        DateTime date = defenseFool.NewDate();
+                    DateTime date = defenseFool.NewDate();
 
-            //        manager.DeleteTask(message, date);
-            //        manager.Save();
-            //    }
-            //    else if (choice == "4")
-            //    {
-            //        manager.SetTasks(TaskManager.Load());
+                    manager.DeleteTask(message, date);
+                    manager.Save();
+                }
+                else if (choice == "4")
+                {
+                    manager.SetTasks(TaskManager.Load());
 
-            //        manager.ReadTasks();
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Вы ввели неверное значение!");
-            //        Console.Clear();
-            //    }
-            //}
-
-            TaskManager manager = new TaskManager();
-            DateTime dateTime = DateTime.Now;
-            manager.AddTask("Pon", dateTime);
-            manager.Save();
-
+                    manager.ReadTasks();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное значение!");
+                    Console.Clear();
+                }
+            }
         }
     }
 }
